3aa35d1128d8062b1362eaf7304377307173c0a6
Igor Babych <igor.babych@adevinta.com>
2025-02-11 15:33:31 +0100

    Adding different languages to a greatings

--- hello_test.go
+++ hello_test.go
@@ -5,13 +5,23 @@ import "testing"
 // The TestHello function is testing the return value of the Hello function.
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Claude", "French")
+		want := "Salut, Claude"
 		assertCorrectMessage(t, got, want)
 	})
 }
