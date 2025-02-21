7a290afe47dae68e4641bd71af3316a5af208915
Igor Babych <igor.babych@adevinta.com>
2025-02-21 12:00:19 +0100

    Get up to first explanation of the Method

--- structMethodInterfaces/structMethodInterfaces.go
+++ structMethodInterfaces/structMethodInterfaces.go
@@ -1,9 +1,22 @@
 package structmethodinterfaces
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return 0
+}
+
+func (c Circle) Area() float64 {
+	return 0
 }
