cf31d7c8bf89e1b8923a3a62a3266ebcce64ee2f
Igor Babych <igor.babych@adevinta.com>
2025-05-15 11:28:00 +0200

    [pointersAndErrors] Up to withdraw

--- pointersAndErrors/pointersAndErrors_test.go
+++ pointersAndErrors/pointersAndErrors_test.go
@@ -7,14 +7,14 @@ import (
 func TestWallet(t *testing.T) {
 	t.Run("Test Wallet", func(t *testing.T) {
 		wallet := Wallet{}
-		wallet.Deposit(10)
+		wallet.Deposit(Bitcoin(10))
 
 		got := wallet.Balance()
 
 		want := Bitcoin(10)
 
 		if got != want {
-			t.Errorf("got %d want %d", got, want)
+			t.Errorf("got %s want %s", got, want)
 		}
 	})
 }
